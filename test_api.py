f9d8690179ae1fefe1acd20883e4edb487cb99dd
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2021-03-03 23:22:11 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -5,14 +5,17 @@ Created on Mon Mar  1 18:39:26 2021
 @author: Tamas
 """
 
-from flask import Flask,request,jsonify,abort
+from flask import Flask,request,jsonify,abort,Response
+from time import sleep
+import pandas as pd
+import numpy as np
 # import json
 # %%init
 app = Flask(__name__)
 
 
 
-# %%endpoint1
+# %%simplejson
 
 @app.route("/getjson")
 def returnjson():
@@ -53,12 +56,21 @@ def returnjson():
 		}
 }
     return x
-# %%endpoint2
+# %%sleepreturn
+@app.route("/wait")
+def wait():
+    sleep(30)
+    return "OK"
 
+# %%csv
+@app.route("/csv")
+def getCsv():
+    
+    df = pd.DataFrame(np.random.randint(0,10000,size=(100,100)))
 
-
+    return Response(df.to_csv(),mimetype="text/csv",headers={"Content-disposition":"attachment; filename=myname.csv"})
 
 #%%Main
 if __name__ == '__main__':    
 
-    app.run(host='127.0.0.1',port=5000)
+    app.run(threaded=False,host='127.0.0.1',port=5000)
